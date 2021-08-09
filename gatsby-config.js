d1e280da0c4bbda1aeb9c6a2fc03d47ad55ccfba
mywin <showweb1@gmail.com>
2021-08-09 18:36:00 +0900

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,27 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
-  },
-  plugins: [
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+	siteMetadata: {
+		siteUrl: "https://www.yourdomain.tld",
+		title: "My First Gatsby Site",
+	},
+	plugins: [
+		"gatsby-plugin-gatsby-cloud",
+		"gatsby-plugin-image",
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		// {
+		// 	resolve: "gatsby-source-filesystem",
+		// 	options: {
+		// 		name: "images",
+		// 		path: "./src/images/",
+		// 	},
+		// 	__key: "images",
+		// },
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: `blog`,
+				path: `${__dirname}/blog`,
+			},
+		},
+	],
 };
